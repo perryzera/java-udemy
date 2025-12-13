7f67d2e3fcea45753e7d5ad064db60c3b33f2905
João Vitor <107291688+perryzera@users.noreply.github.com>
2025-12-13 17:31:17 -0300

    m01.

---
+++ java-udemy/src/m01_estrutura_sequencial/a02_entrada_dados.java
@@ -0,0 +1,11 @@
+package m01_estrutura_sequencial;
+
+import java.util.Scanner;
+
+public class a02_saida_dados {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
